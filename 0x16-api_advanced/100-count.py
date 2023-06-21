dc229f7e360a6ecb7956520a9e2c8dba63a4929e
Abel Ben <abelmorrisben@icloud.com>
2023-06-21 10:40:00 +0000

    100 count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,60 +1,38 @@
-from requests import get
-import re
-
-REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'my-app/0.0.1'}
+import requests
 
 
-def count_words(subreddit, word_list, after="", word_dic=None):
-    if word_dic is None:
-        word_dic = {}
-
-    if not word_dic:
-        for word in word_list:
-            word_dic[word.lower()] = 0
+def count_words(subreddit, word_list, after='', word_dict=None):
+    if word_dict is None:
+        word_dict = {word.lower(): 0 for word in word_list}
 
     if after is None:
-        word_list = [[key, value] for key, value in word_dic.items()]
-        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
-        for w in word_list:
-            if w[1]:
-                print("{}: {}".format(w[0].lower(), w[1]))
+        sorted_words = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_words:
+            if count > 0:
+                print(f"{word}: {count}")
         return None
 
-    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {'user-agent': 'redquery'}
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers, params=parameters, allow_redirects=False)
 
-    params = {
-        'limit': 100,
-        'after': after
-    }
-
-    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
-
-    if r.status_code != 200:
+    if response.status_code != 200:
         return None
 
     try:
-        js = r.json()
-    except ValueError:
+        data = response.json()['data']
+        hot = data['children']
+        aft = data['after']
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in word_dict:
+                if word in lower:
+                    word_dict[word] += lower.count(word)
+
+    except Exception:
         return None
 
-    try:
-        data = js.get("data")
-        after = data.get("after")
-        children = data.get("children")
-        for child in children:
-            post = child.get("data")
-            title = post.get("title")
-            lower = [s.lower() for s in title.split(' ')]
-
-            for w in word_list:
-                word = w[0].lower()
-                count = lower.count(word)
-                if count > 0:
-                    if not re.search(r'\W$', word):
-                        word_dic[word] += count
-
-    except:
-        return None
-
-    count_words(subreddit, word_list, after, word_dic)
+    count_words(subreddit, word_list, aft, word_dict)
