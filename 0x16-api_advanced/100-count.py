870431b6ab58773143b1cd3768f5f91dea587a3e
Abel Ben <abelmorrisben@icloud.com>
2023-06-21 10:12:47 +0000

    100 count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,28 +1,24 @@
-#!/usr/bin/python3
-""" Count it! """
 from requests import get
 
 REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'my-app/0.0.1'}
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
-def count_words(subreddit, word_list, after="", word_dic={}):
-    """
-    Returns a list containing the titles of all hot articles for a
-    given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-    """
+def count_words(subreddit, word_list, after=None, word_dic=None):
+    if word_dic is None:
+        word_dic = {}
+
     if not word_dic:
         for word in word_list:
-            word_dic[word] = 0
+            word_dic[word.lower()] = 0
 
     if after is None:
-        word_list = [[key, value] for key, value in word_dic.items()]
-        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
-        for w in word_list:
-            if w[1]:
-                print("{}: {}".format(w[0].lower(), w[1]))
-        return None
+        sorted_words = sorted(word_dic.items(), key=lambda x: (-x[1], x[0]))
+        results = []
+        for word, count in sorted_words:
+            if count > 0:
+                results.append("{}: {}".format(word, count))
+        return results
 
     url = REDDIT + "r/{}/hot/.json".format(subreddit)
 
@@ -38,24 +34,20 @@ def count_words(subreddit, word_list, after="", word_dic={}):
 
     try:
         js = r.json()
-
     except ValueError:
         return None
 
     try:
-
         data = js.get("data")
         after = data.get("after")
         children = data.get("children")
         for child in children:
             post = child.get("data")
-            title = post.get("title")
-            lower = [s.lower() for s in title.split(' ')]
-
-            for w in word_list:
-                word_dic[w] += lower.count(w.lower())
-
+            title = post.get("title").lower()
+            for word in word_list:
+                if ' ' + word.lower() + ' ' in title:
+                    word_dic[word.lower()] += 1
     except:
         return None
 
-    count_words(subreddit, word_list, after, word_dic)
+    return count_words(subreddit, word_list, after, word_dic)
